d5d3b662d272b0bae6fe3eba6f4f1e554f763e12
thriftthread <the.siddique.umar@gmail.com>
2025-06-26 17:39:12 +0530

    Create users.js

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+// Register/Login (simple version)
+router.post("/login", async (req, res) => {
+  const { phone } = req.body;
+  let user = await User.findOne({ phone });
+
+  if (!user) {
+    user = await User.create({ phone });
+  }
+
+  res.json({ userId: user._id, message: "Login success!" });
+});
