be513c0f0dfe9a32fead9c53ccd5d5cfc94efb45
thriftthread <the.siddique.umar@gmail.com>
2025-06-26 16:04:26 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -4,10 +4,17 @@ const orderSchema = new mongoose.Schema({
   items: Array,
   total: Number,
   customer: {
-    name: String,
-    email: String,
-    address: String
-  },
+  name: String,
+  email: String,
+  phone: String,
+  address: {
+    state: String,
+    city: String,
+    pincode: String,
+    line: String
+  }
+}
+,
   createdAt: {
     type: Date,
     default: Date.now
