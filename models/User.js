b2a360d2d5a9dcbce6aecd1a0c02f301a29c521a
thriftthread <the.siddique.umar@gmail.com>
2025-06-30 07:59:51 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: String,
+  uid: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
   cart: {
     type: [Object],
     default: []
-  }
+  },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("User", userSchema);
